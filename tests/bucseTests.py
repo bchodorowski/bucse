8d8a5f22791093c1af4e660888b4aff352e44b94
Bartosz Chodorowski <bartosz@chodorowski.pro>
2024-09-17 23:31:50 +0200

    Test: random directories and files

--- tests/bucseTests.py
+++ tests/bucseTests.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import time
+import random
+import string
 
 pid = os.getpid()
 tmpFiles = []
@@ -48,7 +50,7 @@ def verifyWithMirror():
     p = subprocess.run(["sync"])
     p.check_returncode()
 
-    p = subprocess.run(["diff", "test_%d_mirror" % pid, "test_%d" % pid])
+    p = subprocess.run(["diff", "-r", "test_%d_mirror" % pid, "test_%d" % pid])
     p.check_returncode()
 
     p = subprocess.run(["umount", "test_%d" % pid])
@@ -57,8 +59,9 @@ def verifyWithMirror():
     p = subprocess.run(["../bucse-mount", "-r", "test_%d_repo" % pid, "test_%d" % pid])
     p.check_returncode()
 
-    p = subprocess.run(["diff", "test_%d_mirror" % pid, "test_%d" % pid])
+    p = subprocess.run(["diff", "-r", "test_%d_mirror" % pid, "test_%d" % pid])
     p.check_returncode()
+
 
 def testCleanup():
     time.sleep(1)
@@ -79,3 +82,40 @@ def testCleanup():
         p = subprocess.run(["rm", "-rf", "tmp/%s" % tmpFile])
         p.check_returncode()
 
+
+def getRandomPathInMirror():
+    p = subprocess.run(["find", "test_%d_mirror" % pid, "-type", "d"], capture_output = True)
+    p.check_returncode()
+
+    dirs = p.stdout.decode("UTF-8").split("\n")[:-1]
+    return random.choice(dirs)
+
+
+randomCharacters = string.ascii_letters + string.digits
+def getRandomFileName():
+    return ''.join(random.choices(randomCharacters, k=random.randint(1, 64)))
+
+
+def getRandomNewFileName():
+    while True:
+        candidate = getRandomPathInMirror() + "/" + getRandomFileName()
+        if not os.path.exists(candidate):
+            return candidate.replace("test_%d_mirror" % pid, "__TESTDIR__", 1)
+
+def getRandomExistingFileName():
+            return getRandomPathInMirror().replace("test_%d_mirror" % pid, "__TESTDIR__", 1)
+
+def makeRandomTmpFile():
+    fileSize = random.randint(0, 1024*1024)
+    fileName = ""
+    while os.path.exists("tmp/" + fileName):
+        fileName = getRandomFileName()
+
+    fileName = getRandomFileName()
+
+    p = subprocess.run(["dd", "bs=1", "count=%d"%fileSize, "if=/dev/random", "of=tmp/%s"%fileName, "status=none"])
+    p.check_returncode()
+
+
+    tmpFiles.append(fileName)
+    return fileName
--- tests/test1.py
+++ tests/test1.py
@@ -7,7 +7,17 @@ bucseTests.downloadTmp(
         'https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-6.9.4.tar.xz',
         'linux-6.9.4.tar.xz',
         '8e4ef26770b4ce8db7d988fe9c9e721762f30a36')
+
 bucseTests.mountDirs()
-bucseTests.mirrorCommand(["cp", "tmp/linux-6.9.4.tar.xz" ,"__TESTDIR__/"])
+
+bucseTests.mirrorCommand(["cp", "tmp/linux-6.9.4.tar.xz", "__TESTDIR__/"])
+
+for _ in range(128):
+    bucseTests.mirrorCommand(["mkdir", bucseTests.getRandomNewFileName()])
+for _ in range(5):
+    fileName = bucseTests.makeRandomTmpFile()
+    targetDir = bucseTests.getRandomExistingFileName()
+    bucseTests.mirrorCommand(["cp", "tmp/%s"%fileName, "%s/"%targetDir])
+
 bucseTests.verifyWithMirror()
 bucseTests.testCleanup()
